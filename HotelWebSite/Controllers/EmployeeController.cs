c7ab29d2e7a540e26956b6a5b297e2ea568100c7
Amir375 <31389709+Amir375@users.noreply.github.com>
2017-11-26 00:40:08 +0330

    Insert and Select Added

---
+++ HotelWebSite/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Hotels.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelWebSite.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            HotelDb ctx = new HotelDb();
+            var Emp = ctx.Employees.ToList();
+
+            return View(Emp);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            HotelDb ctx = new HotelDb();
+            ctx.Employees.Add(employee);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
