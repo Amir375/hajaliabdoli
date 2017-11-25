c170b6558d51cef9b842712a51907ae3f8a55897
Amir375 <31389709+Amir375@users.noreply.github.com>
2017-11-25 23:03:44 +0330

    Create Part 1 Database

--- Hotels.Model/Person.cs
+++ Hotels.Model/Person.cs
@@ -10,7 +10,6 @@ namespace Hotels.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string MidelName { get; set; }
         public string Family { get; set; }
         public string Age { get; set; }
         public string NationalCode { get; set; }
